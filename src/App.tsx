f73662f7ac32f27f651fb4f3beb34e271b8a5288
Carlos Costa <carllos.nc@gmail.com>
2026-01-07 19:28:13 -0300

    fix: title style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <section className="w-full min-h-dvh max-w-[700px] pt-[40px] justify-start flex flex-col gap-[20px] px-[20px]">
 
         <div className="text-center flex flex-col items-center gap-[20px]">
-          <h1 className="text-[30px] font-light">PRONO</h1>
+          <h1 className="text-[30px] font-bold">PRONO</h1>
           <span className="text-neutral-400 block w-full max-w-[300px]">
             An easy and fast way to check your english pronounciation.
           </span>
